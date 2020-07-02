1f923031e8e23bbe8de4031cda160a7cd0f3b95e
parissak <sakari.parisaari@helsinki.fi>
2020-07-02 11:20:40 +0300

    kurssitiedot 1.2

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -10,13 +10,12 @@ const App = () => {
   const part3 = 'State of a component'
   const exercises3 = 14 
   
-  return (
-     
+  return ( 
     <div>
       <Header course = {course}/>
-      <Content part = {part1} exercises = {exercises1}/>
-      <Content part = {part2} exercises ={exercises2}/>
-      <Content part = {part3} exercises ={exercises3}/>
+      <Content part1 = {part1} exercises1 = {exercises1}
+        part2 = {part2} exercises2 = {exercises2}
+        part3 = {part3} exercises3 = {exercises3}/>
       <Total sum = {exercises1 + exercises2 + exercises3}/>     
     </div>
   )
@@ -32,7 +31,11 @@ const Header = (props) => {
 
 const Content = (props) => {
   return(    
-    <p>{props.part} {props.exercises}</p>
+    <div>
+      <p>{props.part1} {props.exercises1}</p>
+      <p>{props.part2} {props.exercises2}</p>
+      <p>{props.part3} {props.exercises3}</p>
+    </div>
   )
 }
 
