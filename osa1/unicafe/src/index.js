62cf6621659b516b74ad9595a642b97c192164a6
parissak <sakari.parisaari@helsinki.fi>
2020-07-03 18:07:02 +0300

    unicafe 1.6

---
+++ osa1/unicafe/src/index.js
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+ 
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const increaseGood = () => setGood(good + 1)
+  const increaseNeutral = () => setNeutral(neutral + 1)
+  const increaseBad = () => setBad(bad + 1)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={increaseGood}> good</button>
+      <button onClick={increaseNeutral}>neutral</button>
+      <button onClick={increaseBad}>bad</button>
+
+      <h1> statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
