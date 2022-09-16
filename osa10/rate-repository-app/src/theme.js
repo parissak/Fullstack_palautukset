10b8d067866f7eab445247d2528fd758d6c1c8e4
SP <sapak@protonmail.com>
2022-09-16 20:37:20 +0300

    exercise 10.10

--- osa10/rate-repository-app/src/theme.js
+++ osa10/rate-repository-app/src/theme.js
@@ -1,3 +1,5 @@
+import { Platform } from 'react-native';
+
 const theme = {
 	colors: {
 		textPrimary: '#24292e',
@@ -12,7 +14,11 @@ const theme = {
 		subheading: 16,
 	},
 	fonts: {
-		main: 'normal',
+		main: Platform.select({
+			android: 'Roboto',
+			ios: 'Arial',
+			default: 'normal',
+		})
 	},
 	fontWeights: {
 		normal: '400',
