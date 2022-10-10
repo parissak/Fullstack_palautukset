c97ddaff08734d935ca90f4630ad9eebce3331d7
SP <sapak@protonmail.com>
2022-10-10 12:04:33 +0300

    update user's reviews when a new one is created

--- osa10/rate-repository-app/src/hooks/useReview.js
+++ osa10/rate-repository-app/src/hooks/useReview.js
@@ -1,9 +1,10 @@
 import { useMutation } from '@apollo/client';
 
 import { CREATE_REVIEW } from '../graphql/queries';
+import { GET_CURRENT_USER_REVIEWS } from '../graphql/queries';
 
 const useReview = () => {
-	const [mutate, result] = useMutation(CREATE_REVIEW);
+	const [mutate, result] = useMutation(CREATE_REVIEW, {refetchQueries: [ { query: GET_CURRENT_USER_REVIEWS } ]});
 
 	const review = async ({ ownerName, repositoryName, rating, text }) => {
 		const {data} = await mutate({ variables: { ownerName, repositoryName, rating, text } });
